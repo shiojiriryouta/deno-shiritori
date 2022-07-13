eb6e73b1e7bf27970cf7c85abe15b46b38b8f507
shiojiri <ojiri.pikachu@gmail.com>
2022-07-13 20:49:51 +0900

    機能を追加しました

--- server.js
+++ server.js
@@ -1,27 +1,67 @@
 import { serve } from "https://deno.land/std@0.138.0/http/server.ts"
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
+function check(str){
+  if (str.match(/^[ぁ-んー　]*$/)) {      
+    return false;
+  } else {
+    return true;
+  }
+}
 let previousWord = "しりとり";
+//過去に出てきた文字列を保存
+let wordlog = [];
+let randomword = ["あたま","つまさき","しょうげき","いなずま","おれたち","みみ","ゆさぶり","おと","いたずら","みのり","かじつ","やばい","ぼると","じだい","いま","びーと"];
+let num = Math.floor(Math.random() * randomword.length) ;
+previousWord = randomword[num];
 console.log("Listening on http://localhost:8000");
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
   console.log(pathname);
   if (req.method === "GET" && pathname === "/shiritori") {
-    return new Response(previousWord);
+    //画面に文字を表示してる
+    return new Response(`これは上\n前の単語:${previousWord}\n[単語の履歴]\n${wordlog.join("\n")}`);
   }
   if (req.method === "POST" && pathname === "/shiritori") {
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
-    if (nextWord.length > 0 &&previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)) {
+    
+    ////各種条件を判定する領域
+    //送信した文字がひらがなか判定
+    let regexp = /^[\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]+$/mu;
+    if(!(regexp).test(String(nextWord))){
+      return new Response("単語がひらがなではありません。", { status: 400 });
+    }
+
+    //送信した文字の語尾が前の文字の頭文字とつながっているか
+    //charAtで文字列の特定の文字だけ切り抜いてる
+    if (nextWord.length == 0 || previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)) {
+      //画面に文字を表示している
       return new Response("前の単語に続いていません。", { status: 400 });
     }
+
+    //送信した文字が「ん」で終わっていないか
+    if(nextWord.charAt(nextWord.length - 1) == 'ん'){
+      wordlog
+      return new Response("単語の語尾が「ん」なので終了しました。", { status: 400 });
+    }
+
+    //送信した文字が過去に使われていないか
+    for(let i=0;i<wordlog.length;i++){
+      if(wordlog[i] == nextWord || previousWord == nextWord){
+        return new Response("この単語は過去に使われました。", { status: 400 });
+      }
+    }
+    wordlog[wordlog.length] = previousWord;
     previousWord = nextWord;
-    return new Response(previousWord);
+    return new Response(`これは下\n前の単語:${previousWord}\n[単語の履歴]\n${wordlog}`);
   }
+
+  //Denoの標準ライブラリを使ってpublic フォルダの任意のファイルが
+  //拡張子に合わせて適切な Content-Type ヘッダがセットされて返ってくる
   return serveDir(req, {
     fsRoot: "public",
     urlRoot: "",
     showDirListing: true,
     enableCors: true,
   });
-
 });
